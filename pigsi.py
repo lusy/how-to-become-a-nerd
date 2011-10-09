85e2ad0e6117893b240eeeb6d1bab96780c492e3
lusy <vaseva@mi.fu-berlin.de>
2011-10-09 20:00:54 +0200

    pig talking

---
+++ pigsi.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, saying = argv
+
+print """
+                              --------
+                            < %s >
+         .-~~~~-. |\\\_        --------
+      @_/        /  oo\_    /
+        |    \   \   _(")  /
+         \   /-|  ||'-'
+          \_\   \_\\\ """ % saying
